ee6dd32207c5c50fad8fd2d5af990fc0f2cdea3a
Miguel Galante <mgalante@gmail.com>
2019-05-24 00:04:40 -0300

    Fix piel

--- src/app/visualization/visualization.component.ts
+++ src/app/visualization/visualization.component.ts
@@ -1,15 +1,14 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import {  HttpClient } from '@angular/common/http';
-import { toTypeScript } from '@angular/compiler';
 
 
 
-export interface Photo    {
-  "url": string,
-  "tipo_de_cuerpo": string,
-  "piel": string|string[],
-  "color_de_piel": string
-};
+export interface Photo {
+  url: string;
+  tipo_de_cuerpo: string;
+  piel: string | string[];
+  color_de_piel: string;
+}
 
 @Component({
   selector: 'app-visualization',
@@ -17,70 +16,69 @@ export interface Photo    {
   styleUrls: ['./visualization.component.scss']
 })
 export class VisualizationComponent implements OnInit {
-
-
-
+  data: Photo[] = [];
   tipoDeCuerpo: string[] = [];
   pielV: string[] = [];
-  colorDePiel:string[]=[];
+  colorDePiel: string[] = [];
 
-  onClickTipoCuerpo(tipoDeCuerpo: string){
+  constructor(private httpClient: HttpClient) { }
+
+  onClickTipoCuerpo(tipoDeCuerpo: string) {
     const index = this.tipoDeCuerpo.indexOf(tipoDeCuerpo);
-    if(index != -1){
-      this.tipoDeCuerpo.splice(index,1);
-    }else{
+    if (index !== -1) {
+      this.tipoDeCuerpo.splice(index, 1);
+    } else {
       this.tipoDeCuerpo.push(tipoDeCuerpo);
     }
   }
 
 
-  onClickColorDePiel(colorDePiel: string){
+  onClickColorDePiel(colorDePiel: string) {
     const index = this.colorDePiel.indexOf(colorDePiel);
-    if(index != -1){
-      this.colorDePiel.splice(index,1);
-    }else{
+    if (index !== -1) {
+      this.colorDePiel.splice(index, 1);
+    } else {
       this.colorDePiel.push(colorDePiel);
     }
   }
 
-  onClickPiel(pielV: string){
-   
+  onClickPiel(pielV: string) {
+
     const index = this.pielV.indexOf(pielV);
-    if(index != -1){
-      this.pielV.splice(index,1);
-    }else{
+    if (index !== -1) {
+      this.pielV.splice(index, 1);
+    } else {
       this.pielV.push(pielV);
     }
   }
 
-  constructor(private httpClient: HttpClient) { }
 
-  data: Photo[] = [];
-  get photos(): Photo[]{
+  get photos(): Photo[] {
 
     let photos = this.data;
-    if (this.tipoDeCuerpo.length>0) {
-      photos = photos.filter(photo=> this.tipoDeCuerpo.indexOf(photo.tipo_de_cuerpo)!=-1);
+    if (this.tipoDeCuerpo.length > 0) {
+      photos = photos.filter(photo => this.tipoDeCuerpo.indexOf(photo.tipo_de_cuerpo) !== -1);
     }
 
-    if (this.colorDePiel.length>0) {
-      photos = photos.filter(photo=> this.colorDePiel.indexOf(photo.color_de_piel)!=-1);
+    if (this.colorDePiel.length > 0) {
+      photos = photos.filter(photo => this.colorDePiel.indexOf(photo.color_de_piel) !== -1);
     }
 
     if (this.pielV.length) {
-      photos = photos.filter(photo=>{
-        const photoPiel = [].concat(photo.piel);
-        return photoPiel.indexOf(photo.piel) != -1;
+      photos = photos.filter(photo => {
+        const pieles: string[] = [].concat(photo.piel);
+        for (const piel of pieles) {
+          if (this.pielV.indexOf(piel) !== -1) {
+            return true;
+          }
+        }
+        return false;
       });
     }
-
     return photos;
   }
 
-
   async ngOnInit() {
-    this.data = await this.httpClient.get<Photo[]>("./assets/json.json").toPromise();
+    this.data = await this.httpClient.get<Photo[]>('./assets/json.json').toPromise();
   }
-
-
 }
